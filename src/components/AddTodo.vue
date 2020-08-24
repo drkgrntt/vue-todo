d6a3a04bed3f7344745f223a17afcba71f2a5405
drkgrntt <drkgrntt@gmail.com>
2020-08-23 21:41:49 -0500

    create app

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add todo" />
+      <input type="submit" value="Submit" class="button" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import { v4 } from 'uuid';
+
+export default {
+  name: "AddTodo",
+  methods: {
+    addTodo(event) {
+      event.preventDefault()
+      const newTodo = {
+        // id: v4(),
+        title: this.title,
+        completed: false
+      }
+      // Send up to parent
+      this.$emit('add-todo', newTodo);
+
+      this.title = '';
+    }
+  },
+  data() {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
